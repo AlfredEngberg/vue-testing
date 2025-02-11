42ec17e01f53c5d5e4997965a94775fda7564306
AlfredEngberg <alfred.engberg@elev.ga.ntig.se>
2025-02-11 14:35:41 +0100

    gambaa route

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -4,6 +4,14 @@ import { RouterLink } from 'vue-router'
 
 <template>
     <nav class="navbar">
+        <RouterLink class="logo-text" to="/">
+            <img class="logo-img" src="../assets/img/logo.png" alt="Logotyp">
+        </RouterLink>
+        <RouterLink class="logo-text" to="/">
+            <h1>Mysterybox-Kungen</h1>
+        </RouterLink>
+
+
         <RouterLink class="btn" to="/">Hem</RouterLink>
         <RouterLink class="btn" to="/about">Om</RouterLink>
         <RouterLink class="btn" to="/contact">Kontakt</RouterLink>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Contact from '../views/Contact.vue'
 import Products from '../views/Products.vue'
+import Gamba from '../views/Gamba.vue'
 import path from 'path'
 
 const router = createRouter({
@@ -12,6 +13,7 @@ const router = createRouter({
         { path: '/about', name: 'about', component: About },
         { path: '/contact', name: 'contact', component: Contact },
         { path: '/products', name: 'products', component: Products },
+        { path: '/gamba', name: 'gamba', component: Gamba },
     ]
 })
 
